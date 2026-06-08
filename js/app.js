e9454a30baaf5f9d8992178640257331150bcf3e
grimmsite <grimmsite33@gmail.com>
2026-06-08 17:26:03 +0300

    fix: correct decimal places for all markets based on live Deriv tick data

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ const ATOOL_WS_URL = "wss://ws.binaryws.com/websockets/v3?app_id=36544";
 const MARKETS = {
   'R_10':     { name: 'Volatility 10 Index',       decimals: 3 },
   'R_25':     { name: 'Volatility 25 Index',       decimals: 3 },
-  'R_50':     { name: 'Volatility 50 Index',       decimals: 3 },
+  'R_50':     { name: 'Volatility 50 Index',       decimals: 4 },
   'R_75':     { name: 'Volatility 75 Index',       decimals: 4 },
   'R_100':    { name: 'Volatility 100 Index',      decimals: 2 },
   '1HZ10V':   { name: 'Volatility 10 (1s) Index',  decimals: 2 },
@@ -19,11 +19,11 @@ const MARKETS = {
   '1HZ50V':   { name: 'Volatility 50 (1s) Index',  decimals: 2 },
   '1HZ75V':   { name: 'Volatility 75 (1s) Index',  decimals: 2 },
   '1HZ100V':  { name: 'Volatility 100 (1s) Index', decimals: 2 },
-  'JD10':     { name: 'Jump 10 Index',             decimals: 3 },
-  'JD25':     { name: 'Jump 25 Index',             decimals: 3 },
-  'JD50':     { name: 'Jump 50 Index',             decimals: 3 },
-  'JD75':     { name: 'Jump 75 Index',             decimals: 3 },
-  'JD100':    { name: 'Jump 100 Index',            decimals: 3 },
+  'JD10':     { name: 'Jump 10 Index',             decimals: 2 },
+  'JD25':     { name: 'Jump 25 Index',             decimals: 2 },
+  'JD50':     { name: 'Jump 50 Index',             decimals: 2 },
+  'JD75':     { name: 'Jump 75 Index',             decimals: 2 },
+  'JD100':    { name: 'Jump 100 Index',            decimals: 2 },
 };
 
 const ANALYSIS_MARKETS = ['R_100', 'R_75', 'R_50', 'R_25', 'R_10'];
